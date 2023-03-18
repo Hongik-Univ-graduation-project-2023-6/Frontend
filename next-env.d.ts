4be8ddfc723aebb1336f2edf1bfb8bf93e8b3722
dongtimes2 <dongtimes2@gmail.com>
2023-03-19 02:41:55 +0900

    feat: svg 파일을 읽어들이기 위한 설정

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+declare module '*.svg' {
+  import React from 'react';
+  const svg: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default svg;
+}
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
