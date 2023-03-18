a677e841166d73761e1d165902f81ca2d1874f96
dongtimes2 <dongtimes2@gmail.com>
2023-03-18 23:46:39 +0900

    design: reset css 설정 및, 전역적으로 적용되는 폰트 정보 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '@/styles/globals.css';
+import { reset } from '@/styles/reset';
+import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={reset} />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
