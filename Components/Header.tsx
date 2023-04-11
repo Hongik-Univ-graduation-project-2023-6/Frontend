7c5b0afdc183210b1e9898b5938c29a958515aa5
dongtimes2 <dongtimes2@gmail.com>
2023-04-11 18:29:42 +0900

    feat: prop의 옵션에 따라 헤더에 검색 및 뒤로가기 버튼이 뜨는지의 여부 기능 구현 및 메뉴 모달 장착

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -2,14 +2,44 @@ import { css } from '@emotion/react';
 import Menu from '@/public/assets/svg/menu.svg';
 import Logo from '@/public/assets/svg/logo.svg';
 import Search from '@/public/assets/svg/search.svg';
+import LeftArrow from '@/public/assets/svg/left_arrow.svg';
 import { colors } from '@/styles/colors';
+import { useRouter } from 'next/router';
+import { PATH } from '@/constants/path';
+import MenuModal from './MenuModal';
+import { useState } from 'react';
 
-const Header = () => {
+interface Props {
+  showPrevButton?: boolean;
+  showSearchButton?: boolean;
+}
+
+const Header = ({ showPrevButton, showSearchButton }: Props) => {
+  const [showModal, setShowModal] = useState(false);
+  const router = useRouter();
+
+  const handlePrevButtonClick = () => {
+    router.back();
+  };
+
+  const handleSearchButtonClick = () => {
+    router.push(PATH.SEARCH);
+  };
+
+  const handleMenuButtonClick = () => {
+    setShowModal(true);
+  };
+
   return (
     <div css={wrapper}>
-      <Menu />
+      <MenuModal showModal={showModal} setShowModal={setShowModal} />
+      {showPrevButton ? (
+        <LeftArrow onClick={handlePrevButtonClick} />
+      ) : (
+        <Menu onClick={handleMenuButtonClick} />
+      )}
       <Logo />
-      <Search />
+      {showSearchButton && <Search onClick={handleSearchButtonClick} />}
     </div>
   );
 };
@@ -22,9 +52,17 @@ const wrapper = css`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  height: 6rem;
   padding: 3.5rem 1rem 0.75rem;
   background-color: ${colors.white};
-  z-index: 999;
+  z-index: 900;
+
+  & > svg:nth-of-type(2) {
+    position: absolute;
+    top: 3.5rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `;
 
 export default Header;
