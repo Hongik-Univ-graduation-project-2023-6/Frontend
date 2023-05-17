f3f83b5d7ecb28d1ef73623eaf4c1123d15af489
dongtimes2 <dongtimes2@gmail.com>
2023-05-17 09:42:46 +0900

    feat: 글쓰기 페이지 구현

---
+++ pages/write/index.tsx
@@ -0,0 +1,29 @@
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import Header from '@/components/Header';
+import { HEADER_HEIGHT, SIDE_PADDING } from '@/constants/layout';
+import { PATH } from '@/constants/path';
+import Editor from './components/editor';
+
+const WritePage = () => {
+  const router = useRouter();
+
+  return (
+    <div css={layout}>
+      <Header />
+      <Editor
+        onCancelButtonClick={() => router.push(PATH.MAIN)}
+        onSubmitButtomClick={() => null}
+      />
+    </div>
+  );
+};
+
+const layout = css`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: ${HEADER_HEIGHT} ${SIDE_PADDING} 0;
+`;
+
+export default WritePage;
